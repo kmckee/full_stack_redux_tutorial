43c5d87106cdb46044fb29f803d385f8feb0542a
Kyle McKee <mckee.kyle@gmail.com>
2015-11-14 09:08:25 -0500

    immutable list passes

--- voting-server/test/immutable_spec.js
+++ voting-server/test/immutable_spec.js
@@ -1,21 +1,41 @@
 import {expect} from 'chai';
+import {List} from 'immutable';
 
 describe('immutability', () => {
 
-  describe('a number', () => {
+    describe('a number', () => {
+        function increment(currentState) {
+            return currentState + 1;
+        }
 
-    function increment(currentState) {
-      return currentState + 1;
-    }
+        it('is immutable', () => {
+            let state = 42;
+            let nextState = increment(state);
 
-    it('is immutable', () => {
-      let state = 42;
-      let nextState = increment(state);
-
-      expect(nextState).to.equal(43);
-      expect(state).to.equal(42);
+            expect(nextState).to.equal(43);
+            expect(state).to.equal(42);
+        });
     });
 
-  });
+    describe('A List', () => {
+        function addMovie(currentState, movie) {
+            return currentState.push(movie);
+        }
+
+        it('is immutable', () => {
+            let state = List.of('Trainspotting', '28 Days Later');
+            let nextState = addMovie(state, 'Sunshine');
+
+            expect(nextState).to.equal(List.of(
+                        'Trainspotting',
+                        '28 Days Later',
+                        'Sunshine'
+                        ));
+            expect(state).to.equal(List.of(
+                        'Trainspotting',
+                        '28 Days Later'
+                        ));
+        });
+    });
 
 });
