86063a60008cf99f2db1c79d145b64d6ddda51a5
Kyle McKee <mckee.kyle@gmail.com>
2015-11-15 12:57:19 -0500

    Reducer composition - limiting state exposed to actions

--- voting-server/test/core_spec.js
+++ voting-server/test/core_spec.js
@@ -41,44 +41,35 @@ describe('application logic', () => {
     describe('vote', () => {
         it('creates a tally for the voted enty', () => {
             const state = Map({
-                vote: Map({
-                    pair: List.of('Trainspotting', '28 Days Later')
-                }),
-                entries: List()
+                pair: List.of('Trainspotting', '28 Days Later')
             });
             const nextState = vote(state, 'Trainspotting');
-            expect(nextState).to.equal(Map({
-                vote: Map({
-                    pair: List.of('Trainspotting', '28 Days Later'),
-                    tally: Map({
-                        'Trainspotting': 1
+            expect(nextState).to.equal(
+                    Map({
+                        pair: List.of('Trainspotting', '28 Days Later'),
+                        tally: Map({
+                            'Trainspotting': 1
+                        })
                     })
-                }),
-                entries: List()
-            }));
+                    );
         });
         it('adds to existing tally for the voted entry', () => {
             const state = Immutable.fromJS({
-                vote: {
-                    pair: ['Trainspotting', '28 Days Later'],
-                    tally: {
-                        'Trainspotting': 3,
-                        '28 Days Later': 2
-                    }
-                },
-                entries: []
+                pair: ['Trainspotting', '28 Days Later'],
+                tally: {
+                    'Trainspotting': 3,
+                    '28 Days Later': 2
+                }
             });
             const nextState = vote(state, 'Trainspotting');
-            expect(nextState).to.equal(Immutable.fromJS({
-                vote: {
-                    pair: ['Trainspotting', '28 Days Later'],
-                    tally: {
-                        'Trainspotting': 4,
-                        '28 Days Later': 2
-                    }
-                },
-                entries: []
-            }));
+            expect(nextState.toJS()).to.eql({
+                pair: ['Trainspotting', '28 Days Later'],
+                tally: {
+                    'Trainspotting': 4,
+                    '28 Days Later': 2
+
+                }
+            });
 
         });
         describe('moving to the next pair', () => {
