25ad1f33d4df60a7e2bdf701f67abc21b7798a89
Kyle McKee <mckee.kyle@gmail.com>
2015-11-21 09:47:11 -0500

    Added voting component, first test passes.

---
+++ voting-client/test/components/Voting_spec.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {renderIntoDocument, scryRenderedDOMComponentsWithTag} from 'react-addons-test-utils';
+import {expect} from 'chai';
+
+import Voting from '../../src/components/Voting';
+
+describe('Voting', () => {
+    it('renders a pair of buttons', () => {
+        const component = renderIntoDocument(<Voting pair={['Trainspotting', '28 Days Later']} />);
+
+        const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+        expect(buttons[0].textContent).to.equal('Trainspotting');
+        expect(buttons[1].textContent).to.equal('28 Days Later');
+    });
+});
